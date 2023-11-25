85e67e5249e9f3830ad803b075f257de763b6f52
sunwoong <sunwoong1122@gmail.com>
2023-11-26 01:16:11 +0900

    [feat] #2 케이크 주인 아이디 조회 API 구현

--- src/main/java/org/sopt/sopkathon/domain/user/repository/UserRepository.java
+++ src/main/java/org/sopt/sopkathon/domain/user/repository/UserRepository.java
@@ -5,8 +5,17 @@ import org.sopt.sopkathon.global.error.BusinessException;
 import org.sopt.sopkathon.global.error.ErrorStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByName(String name);
+
+    Optional<User> findByName(String name);
+
+    default User findByNameOrThrow(String name) {
+        return findByName(name)
+                .orElseThrow(() -> new BusinessException(ErrorStatus.USER_NOT_FOUND));
+    }
 
     default User findByIdOrThrow(Long userId) {
         return findById(userId)
