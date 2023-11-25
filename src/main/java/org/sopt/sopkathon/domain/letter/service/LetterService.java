cb7e1bd9d3607d9c45842523de0b6fcd85630397
tkdwns414 <sangjune2000@naver.com>
2023-11-26 04:02:12 +0900

    [feat] #13 add sprinkeLetter logic with emailProvider

--- src/main/java/org/sopt/sopkathon/domain/letter/service/LetterService.java
+++ src/main/java/org/sopt/sopkathon/domain/letter/service/LetterService.java
@@ -10,6 +10,9 @@ import org.sopt.sopkathon.domain.letter.dto.response.LettersColorResponse;
 import org.sopt.sopkathon.domain.letter.repository.LetterRepository;
 import org.sopt.sopkathon.domain.user.domain.User;
 import org.sopt.sopkathon.domain.user.repository.UserRepository;
+import org.sopt.sopkathon.global.common.EmailMessage;
+import org.sopt.sopkathon.global.smtp.EmailProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,9 +22,11 @@ import java.util.List;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class LetterService {
-
+    @Value("${email.letter-domain}")
+    private String letterUrl;
     private final LetterRepository letterRepository;
     private final UserRepository userRepository;
+    private final EmailProvider emailProvider;
     @Transactional
     public void createLetter(LetterCreateRequest request){
         User user = userRepository.findByIdOrThrow(request.userId());
@@ -41,5 +46,17 @@ public class LetterService {
         User user = userRepository.findByIdOrThrow(letter.getUser().getId());
         return LetterDetailResponse.of(
                 letter.getName(), letter.getColor(), letter.getContent(), user.getName());
+    }
+
+    public void sprinkleLetter(LettersColorRequest request) {
+        User user = userRepository.findByIdOrThrow(request.userId());
+        String name = user.getName();
+        List<Letter> letters = user.getLetters();
+        for (Letter letter : letters) {
+            emailProvider.sendMail(letter.getToEmail(),
+                    String.format(EmailMessage.LETTER_TITLE.getMessage()),
+                    String.format(EmailMessage.LETTER_CONTENT.getMessage(),
+                            name, letterUrl + letter.getId(), name, name));
+        }
     }
 }
