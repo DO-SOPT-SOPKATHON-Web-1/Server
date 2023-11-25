a79606b48d52c4037a2a6f368afdb6e4e418a683
tkdwns414 <sangjune2000@naver.com>
2023-11-26 04:01:46 +0900

    [feat] #13 fix text in LETTER_CONTENT

--- src/main/java/org/sopt/sopkathon/global/common/EmailMessage.java
+++ src/main/java/org/sopt/sopkathon/global/common/EmailMessage.java
@@ -14,7 +14,7 @@ public enum EmailMessage {
             "%s\n" +
             "이 편지는 고인이 생전에 사랑했던 이들이 죽음에 슬퍼하는 대신, 고인을 행복하게 추억할 수 있도록 미리 보내 둔 편지입니다.\n" +
             "%s님의 죽음에 슬퍼만 하기 보다는, 그와의 행복한 추억을 떠올려 보시는 건 어떨까요?\n" +
-            "%s님의 HAPPY DEATH DAY를 위해. %s 드림."),
+            "%s님의 HAPPY DEATH DAY를 위해. HDD 드림."),
     MATCHES_TITLE("HAPPYDEATHDAY의 성냥이 되셨어요!"),
     MATCHES_CONTENT("안녕하세요.\n" +
             "HAPPYDEATHDAY입니다\uD83D\uDE0A\n" +
